b48f965a93246bb5c7916b4d2badc23a4db9d242
Mahir-S <mahirshah148@gmail.com>
2018-04-25 05:08:40 +0530

    stage two

--- book/views.py
+++ book/views.py
@@ -1,24 +1,13 @@
 from django.shortcuts import render
 from .models import Match
 from datetime import datetime as dt
-# Create your views here.
+
 def home(request):
 	return render(request, 'book/home.html',)
-	#return render(request,'book/home.html')
 
 def match(request):
 	    try:
-	    	
-	    	matchlist = Match.objects.filter(match_date__gt=dt.now()).order_by('match_date')
-	    	
-	        
+	    	matchlist = Match.objects.filter(match_date__gt=dt.now()).order_by('match_date')    
 	    except:
 	        matchlist = []
 	    return render(request, 'book/match.html', {'matchlist': matchlist})
-"""
-def match(request):
-	print("HI")
-	matchlist = Match.objects.filter(match_date__gt=dt.now()).order_by('match_date')
-	print(matchlist)
-	return render(request, 'book/match.html', {'matchlist': matchlist})
-"""
